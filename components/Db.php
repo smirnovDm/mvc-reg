f8b01bcefd9bda816df07b154b9d17eedd0f55d8
smirnovDm <smirnov.dm31@gmail.com>
2019-05-28 12:54:26 +0300

    PDO change

--- components/Db.php
+++ components/Db.php
@@ -1,19 +1,18 @@
 <?php
 
-
-
 namespace components;
+
 use mysqli;
+use PDO;
+
 class Db {
-
-
 
     public static function getConnection() {
         $paramsPath = ROOT . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
         include($paramsPath);
-     $db = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
-     return $db;
-       
+        $db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PWD);
+//        $db = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+        return $db;
     }
 
 }
--- models/News.php
+++ models/News.php
@@ -1,24 +1,20 @@
 <?php
 
 include_once ROOT . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'Db.php';
+
 use components\Db;
 
 class News {
 
-    
-
     public static function getNewsList() {
         $db = Db::getConnection();
-        
-        
         $query = "SELECT * FROM " . 'news' . ";";
         $result = $db->query($query);
-        
+
         if (!$result) {
             return false;
         }
-        return $result->fetch_all(MYSQLI_ASSOC);
-        
+      return $result->fetchAll((PDO::FETCH_ASSOC));
     }
 
 }
